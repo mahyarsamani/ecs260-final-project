ff218312be4de06dcf1556bbbe964dd9e3457a83
AliciaS <aliciasiu.98@gmail.com>
2021-11-11 12:45:49 -0800

    resolved conflict

--- code/regression_plots.py
+++ code/regression_plots.py
@@ -6,17 +6,17 @@ from sklearn.linear_model import LinearRegression
 # read_file = pd.read_excel(r'~\ecs260-final-project\metrics_dummydata.xlsx', sheet_name='metrics_dummydata')
 # read_file.to_csv(r'~\ecs260-final-project\metrics_dummydata.csv', index=None, header=True)
 
-data = pd.read_csv('../dataset/metrics_dummydata.csv')  # load data set
+data = pd.read_csv('../dataset/gunrock.csv')  # load data set
 
 ## Performance(num_bugs) vs Productivity(num_commits)
-num_commits = data.iloc[:, 1].values.reshape(-1, 1)  # (Y-value) values converts it into a numpy array
+num_commits = data.iloc[:, 5].values.reshape(-1, 1)  # (Y-value) values converts it into a numpy array
 
-num_bugs = data.iloc[:, 2].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
+num_bugs = data.iloc[:, 1].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(num_bugs, num_commits)  # perform linear regression
 num_commits_pred = linear_regressor.predict(num_bugs)  # make predictions
 
-fig, ax = plt.subplots(3, 2, figsize = (8,8))
+fig, ax = plt.subplots(2, 2, figsize = (8,8))
 ax[0,0].scatter(num_bugs, num_commits)
 ax[0,0].plot(num_bugs, num_commits_pred, color='red')
 ax[0,0].set_title("Performance v.s. Productivity")
@@ -28,7 +28,7 @@ ax[0,0].set_ylabel("Productivity(Commits)")
 # plt.plot(num_bugs, num_commits_pred, color='red')
 
 ## Activity(PR_opened) vs Productivity(num_commits)
-PR_opened = data.iloc[:, 3].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
+PR_opened = data.iloc[:, 2].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(PR_opened, num_commits)  # perform linear regression
 num_commits_pred = linear_regressor.predict(PR_opened)  # make predictions
@@ -45,7 +45,7 @@ ax[0,1].set_ylabel("Productivity(Commits)")
 
 
 ## Activity(PR_closed) vs Productivity(num_commits)
-PR_closed = data.iloc[:, 4].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
+PR_closed = data.iloc[:, 3].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(PR_closed, num_commits)  # perform linear regression
 num_commits_pred = linear_regressor.predict(PR_closed)  # make predictions
@@ -62,7 +62,7 @@ ax[1,0].set_ylabel("Productivity(Commits)")
 
 
 ## Communication(PR_merged) vs Productivity(num_commits)
-PR_merged = data.iloc[:, 5].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
+PR_merged = data.iloc[:, 4].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(PR_merged, num_commits)  # perform linear regression
 num_commits_pred = linear_regressor.predict(PR_merged)  # make predictions
@@ -78,17 +78,17 @@ ax[1,1].set_ylabel("Productivity(Commits)")
 # plt.plot(PR_merged, num_commits_pred, color='red')
 
 
-## Communication(PR_unmerged) vs Productivity(num_commits)
-PR_unmerged = data.iloc[:, 6].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
-linear_regressor = LinearRegression()  # create object for the class
-linear_regressor.fit(PR_unmerged, num_commits)  # perform linear regression
-num_commits_pred = linear_regressor.predict(PR_unmerged)  # make predictions
+# ## Communication(PR_unmerged) vs Productivity(num_commits)
+# PR_unmerged = data.iloc[:, 6].values.reshape(-1, 1)  # (X-value) -1 means that calculate the dimension of rows, but have 1 column
+# linear_regressor = LinearRegression()  # create object for the class
+# linear_regressor.fit(PR_unmerged, num_commits)  # perform linear regression
+# num_commits_pred = linear_regressor.predict(PR_unmerged)  # make predictions
 
-ax[2,0].scatter(PR_unmerged, num_commits)
-ax[2,0].plot(PR_unmerged, num_commits_pred, color='red')
-ax[2,0].set_title("Communication v.s. Productivity")
-ax[2,0].set_xlabel("Communication(Unmerged pull requests)")
-ax[2,0].set_ylabel("Productivity(Commits)")
+# ax[2,0].scatter(PR_unmerged, num_commits)
+# ax[2,0].plot(PR_unmerged, num_commits_pred, color='red')
+# ax[2,0].set_title("Communication v.s. Productivity")
+# ax[2,0].set_xlabel("Communication(Unmerged pull requests)")
+# ax[2,0].set_ylabel("Productivity(Commits)")
 
 # plt.subplot(3, 2, 5)
 # plt.scatter(PR_unmerged, num_commits)
