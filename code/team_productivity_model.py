5388a220b5c11e75410c3faef8b6b39919fb3121
agalup <lupinska.agnieszka@gmail.com>
2021-12-04 00:33:35 +0100

    init team productivity model

---
+++ code/team_productivity_model.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import matplotlib.pyplot as plt  # To visualize
+import statsmodels.api as sm
+from pandas import DataFrame
+
+
+data = DataFrame.read_csv('../dataset/pullreq_ci.csv')
+
+
+
